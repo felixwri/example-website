25c2fb42b04e141884956abd3812f43bc99e5d3f
felixwri <73856380+felixwri@users.noreply.github.com>
2023-01-16 11:34:10 +0000

    Initial commit with flask server

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html', page_name="home")
+
+@app.route('/menu', methods=['GET', 'POST'])
+def menu():
+    print(request.method)
+    return render_template('home.html', page_name="menu")
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
