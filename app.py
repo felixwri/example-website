a0ffc4d0491ac27187ed6a365cf5e0dd30703a84
Amad Chaudhari <amadchaudhari174@gmail.com>
2023-01-17 14:30:28 +0000

    added a responsive navbar using bootstrap

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,  send_from_directory
 
 app = Flask(__name__)
+ 
 
 @app.route('/')
 def home():
     return render_template('home.html', page_name="home")
 
+
+
 @app.route('/menu', methods=['GET', 'POST'])
 def menu():
     print(request.method)
     return render_template('menu.html', page_name="menu")
+
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('templates/css', path)    
+
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('templates/js', path)  
 
 if __name__ == '__main__':
     app.debug = True
