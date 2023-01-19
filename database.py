7fca02b2bf64d1783bb9196676841e9ff4d96a31
Ahmet Cihan <ahmet@Ahmets-MacBook-Pro.local>
2023-01-19 23:09:04 +0000

    Add items, delete item, and update item functions has been created. Added several items to the database.

--- database.py
+++ database.py
@@ -9,21 +9,76 @@ def create_table():
     cursor = connection.cursor()
 
     try:
+        # delete the table to not have any duplication of data
+        cursor.execute(f"DROP TABLE IF EXISTS mains_table")
         # any postgres sql statement can be run by the execute method
         # the result is stored in the cursor object
-        cursor.execute(f"CREATE TABLE IF NOT EXISTS mains_table (name text, price real, description text)")
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS mains_table (id integer, name text, price real, description text)")
 
-        cursor.execute(f"INSERT INTO mains_table (name, price, description) VALUES('Manti', 15.00, 'Small dumplings with yogurt and red souce on top.')")
-
-        print(f"Inserted row(s) into test_table")
+        print(f"Table created.")
 
         # any executed commands need to be commited otherwise they will not be saved
         connection.commit()
+        
 
     except Exception as e:
-        print("Failed to build the test_table")
+        print("Failed to build the mains_table")
         print(e)
         # this will rollback the state of the database to before any changes were made by the cursor
         connection.rollback()
 
+
+
+def add_items(id, name, price, description):
+
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"INSERT INTO mains_table (id, name, price, description) VALUES (%s, %s, %s, %s)", (id, name, price, description))
+        
+        connection.commit()
+
+    except Exception as e:
+        print("Insertion failed.")
+        print(e)
+
+        connection.rollback()
+
+def delete_items(id):
+
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"Delete from mains_table where id = %s", (id))
+
+        connection.commit()
+
+
+    except Exception as e:
+        print("Deletion failed")
+        print(e)
+
+        connection.rollback()
+
+def update_price(price, id):
+    
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"Update mains_table set price = %s where id = %s", (price, id))
+
+        connection.commit()
+
+    except Exception as e:  
+        print("Update failed.")
+        print(e)
+
+        connection.rollback() 
+    
 create_table()
+add_items(1, 'MEXICAN PAELLA', 15.00, 'Slow cooked Mexican rice with tiger prawns, chicken, chorizo, red salsa, peas & sweetcorn. (Hot)')
+add_items(2, 'SEAFOOD PAELLA', 15.00, 'Mexican rice cooked with chorizo, seafood cocktail, black tiger prawns, roasted salsa, peas, sweet corn and fresh coriander. (Hot)')
+add_items(3, 'MEAT PAELLA', 15.00, 'Slow cooked Mexican rice with chicken, chorizo, red salsa, peas & sweetcorn topped with grilled steak & bacon. (Hot)')
+add_items(4, 'BEEF CARNITAS ENCHILADA', 14.50, 'olled flour or white corn tortillas (GF) filled with jack cheese, poblano chilli peppers and topped with creamy salsa, jalapeños & cheese. Oven baked and served over a bed of Mexican rice & our famous guacamole.')
+add_items(5, 'POBLANO CHICKEN', 14.50, 'Grilled chicken breast topped with red salsa, poblano chilli peppers, melted jack cheese, served with seasoned fries.')
+
+
+
+
