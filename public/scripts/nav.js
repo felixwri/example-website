48e8f9ce0db6c14f1464ca54905859ce80a1428c
felixwri <73856380+felixwri@users.noreply.github.com>
2023-01-20 20:18:55 +0000

    Added a menu button to the nav bar

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -11,17 +11,42 @@ function toggleMobileNav() {
 
 // determines if the nav bar is expanded or not
 let navExpandedToggle = true;
+// is the menu button in the nav showing or not
+let navMenuButtonToggle = false;
 
 window.addEventListener("scroll", () => {
+    console.log("fired");
+    dyncamicNavbar();
+    dynamicMenuButton();
+});
+
+function dyncamicNavbar() {
     const navExpandTrigger = 100;
     const y = window.scrollY;
     let nav = document.querySelector("nav");
+    let navTitle = document.querySelector("#nav-title");
 
     if (y > navExpandTrigger && navExpandedToggle) {
-        nav.style.height = "4rem";
+        nav.style.height = "4.5rem";
+        navTitle.style.fontSize = "2rem";
         navExpandedToggle = false;
     } else if (y < navExpandTrigger && !navExpandedToggle) {
         nav.style.height = "8rem";
+        navTitle.style.fontSize = "2.5rem";
         navExpandedToggle = true;
     }
-});
+}
+
+function dynamicMenuButton() {
+    const menuTrigger = 400;
+    const y = window.scrollY;
+    let menu = document.querySelector("#nav-revealed-menu");
+
+    if (y > menuTrigger && navMenuButtonToggle) {
+        menu.style.left = "0%";
+        navMenuButtonToggle = false;
+    } else if (y < menuTrigger && !navMenuButtonToggle) {
+        menu.style.left = "100%";
+        navMenuButtonToggle = true;
+    }
+}
