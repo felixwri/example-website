32e786412b1a6a54ec82618662665eb6b70e9c01
felixwri <73856380+felixwri@users.noreply.github.com>
2023-01-30 01:40:55 +0000

    Indicator for basket price and basket size

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -33,6 +33,18 @@ class Order {
         }
     }
 
+    currentItem() {
+        let current = this.items[this.length() - 1];
+        if (current) {
+            return current;
+        } else {
+            return {
+                id: -1,
+                price: 0.0,
+            };
+        }
+    }
+
     length() {
         return this.items.length;
     }
@@ -143,6 +155,9 @@ function increaseCounter(id) {
     currentValue++;
     counter.innerText = currentValue;
     order.addItem(id);
+
+    increaseQuantity();
+    increasePrice();
 }
 
 function decreaseCounter(id) {
@@ -155,6 +170,141 @@ function decreaseCounter(id) {
     counter.innerText = currentValue;
 
     order.removeItem(id);
+
+    decreaseQuantity();
+    decreasePrice();
+}
+
+function increaseQuantity() {
+    let targetAmount = order.length();
+    // transition in queued element
+    let element = document.querySelector(`.bq-higher`);
+    element.style.top = "0%";
+
+    // transition out current
+    let current = document.querySelector(`.bq-current`);
+    current.style.top = "100%";
+    current.classList.remove("bq-current");
+
+    setTimeout(() => {
+        current.remove();
+    }, 1000);
+
+    // create next element for the queue
+    let node = document.createElement("div");
+    node.classList.add("bq-value");
+    node.classList.add("bq-higher");
+
+    node.innerText = targetAmount + 1;
+    let parent = current.parentNode;
+
+    parent.insertBefore(node, current);
+
+    // update classes of transitioned elements
+    element.classList.remove("bq-higher");
+    element.classList.add("bq-current");
+}
+
+function increasePrice() {
+    let targetPrice = order.totalPrice();
+    // transition in queued element
+    let element = document.querySelector(`.bt-higher`);
+
+    if (!targetPrice.toString().includes(".")) {
+        element.innerText = `£${targetPrice}.0`;
+    } else {
+        element.innerText = `£${targetPrice}`;
+    }
+
+    element.style.top = "0%";
+
+    // transition out current
+    let current = document.querySelector(`.bt-current`);
+    current.style.top = "100%";
+    current.classList.remove("bt-current");
+
+    setTimeout(() => {
+        current.remove();
+    }, 1000);
+
+    // create next element for the queue
+    let node = document.createElement("div");
+    node.classList.add("bt-value");
+    node.classList.add("bt-higher");
+
+    node.innerText = "pend";
+    node.dataset.price = "pending";
+    let parent = current.parentNode;
+
+    parent.insertBefore(node, current);
+
+    // update classes of transitioned elements
+    element.classList.remove("bt-higher");
+    element.classList.add("bt-current");
+}
+
+function decreaseQuantity() {
+    let targetAmount = order.length();
+    // transition in queued element
+    let element = document.querySelector(`.bq-lower`);
+    element.style.top = "0%";
+    element.innerText = targetAmount;
+
+    // transition out current
+    let current = document.querySelector(`.bq-current`);
+    current.style.top = "-100%";
+    current.classList.remove("bq-current");
+
+    let node = document.createElement("div");
+    node.classList.add("bq-value");
+    node.classList.add("bq-lower");
+    let parent = current.parentNode;
+
+    parent.insertBefore(node, current);
+
+    setTimeout(() => {
+        current.remove();
+    }, 1000);
+
+    // update classes of transitioned elements
+    element.classList.remove("bq-lower");
+    element.classList.add("bq-current");
+}
+
+function decreasePrice() {
+    let targetPrice = order.totalPrice();
+    // transition in queued element
+    let element = document.querySelector(`.bt-lower`);
+    element.style.top = "0%";
+
+    if (!targetPrice.toString().includes(".")) {
+        element.innerText = `£${targetPrice}.0`;
+    } else {
+        element.innerText = `£${targetPrice}`;
+    }
+
+    // transition out current
+    let current = document.querySelector(`.bt-current`);
+    current.style.top = "-100%";
+    current.classList.remove("bt-current");
+
+    setTimeout(() => {
+        current.remove();
+    }, 1000);
+
+    // create next element for the queue
+    let node = document.createElement("div");
+    node.classList.add("bt-value");
+    node.classList.add("bt-lower");
+
+    node.innerText = "pend";
+    let parent = current.parentNode;
+
+    parent.insertBefore(node, current);
+
+    // update classes of transitioned elements
+    element.classList.remove("bt-lower");
+    element.classList.add("bt-current");
 }
 
 function scrollToElement(id) {
