285f3f945ed59fae79cba455e50b24b65b2bee19
Amad Chaudhari <amadchaudhari174@gmail.com>
2023-01-31 11:33:07 +0000

    testing connection of postGres database

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,1 +1,13 @@
 from database import *
+
+
+def test_postGres():
+
+    try:
+        connection = psycopg2.connect(
+            "postgres://odstwujyyeqrmq:e17c2c73945aea33a3547fc80fee617794063f339711ba1ffcdf6de4055c10aa@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dai4en0moi3ve4")
+
+        connection.close()
+        assert True
+    except:
+        assert False
