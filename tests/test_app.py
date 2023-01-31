691adbdd550df7414bfef717c85da30505ce7f9c
Amad Chaudhari <amadchaudhari174@gmail.com>
2023-01-31 10:21:01 +0000

    test menu page existenece

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,13 @@ def test_home_page_get_request(client):
     response = client.get('/')
     assert response.status_code == 200, "Route '/' is active"
 
+
+def test_menu_page_get_request(client):
+    response = client.get('/menu')
+    assert response.status_code == 200, "Route '/smenu' is active"
+
+
+
 def test_home_page_title(client):
     response = client.get("/")
     assert b"<title>Oaxaca</title>" in response.data
