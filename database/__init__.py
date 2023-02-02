59fbef55ba6bc029962d422a242d4e209fa2ac00
Ahmet Cihan <ahmet@Ahmets-MacBook-Pro.local>
2023-02-02 16:01:09 +0000

    There is a database for orders now.

--- database/__init__.py
+++ database/__init__.py
@@ -4,6 +4,5 @@ import bcrypt
 connection = psycopg2.connect("postgres://odstwujyyeqrmq:e17c2c73945aea33a3547fc80fee617794063f339711ba1ffcdf6de4055c10aa@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dai4en0moi3ve4")
 
 
-from .login_database import *
-from .drinks_database import *
-from .msd_database import *
+from database.login_database import *
+from database.menu_database import *
--- database/login_database.py
+++ database/login_database.py
@@ -1,5 +1,7 @@
 import psycopg2, bcrypt
-from database import connection
+
+connection = psycopg2.connect("postgres://odstwujyyeqrmq:e17c2c73945aea33a3547fc80fee617794063f339711ba1ffcdf6de4055c10aa@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/dai4en0moi3ve4")
+
 
 def create_users_tabel():
         # create a cursor for navigating the postgres e
