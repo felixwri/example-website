1dd9c698da2ec7979383d50bfe7939c3ae50252f
Amad Chaudhari <amadchaudhari174@gmail.com>
2023-02-20 13:32:24 +0000

    finished implementation of staff Home Page

--- public/scripts/statusPiChart.js
+++ public/scripts/statusPiChart.js
@@ -1,9 +1,26 @@
+const Order = require("./order");
 
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
-
+import Order from "./order";
 function drawChart() {
+
+
+  const orders = JSON.parse(` {{ orders | tojson }} `)
+
+
+  for (order in orders){
+
+    
+    for (item in order["items"]){
+      console.log("amad")
+    }
+  }
+
+
+
+
   var data = google.visualization.arrayToDataTable([
   ['Status', 'Status of order'],
   ['Ordered', 8],
@@ -11,6 +28,13 @@ function drawChart() {
   ['Cooking', 4],
  
 ]);
+
+
+
+
+
+
+
 
   var options = {'title':'Orders', 'width':700, 'height':500};
 
