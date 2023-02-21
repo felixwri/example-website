92f8000ddacfa6c6fda6103f9da9e8eaf0815ccb
felixwri <73856380+felixwri@users.noreply.github.com>
2023-02-21 14:56:27 +0000

    Removed git merge message from __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,12 +1,7 @@
 import psycopg2
 import bcrypt
-<<<<<<< HEAD
-connection = psycopg2.connect("postgres://judajuxuiirttb:2b479b982ee432dbf30816d4f26d87aa3e2c2b458628cba8311be031d295dddb@ec2-52-17-81-195.eu-west-1.compute.amazonaws.com:5432/d9qjkvssoft7iu")
-=======
 
 connection = psycopg2.connect("postgres://judajuxuiirttb:2b479b982ee432dbf30816d4f26d87aa3e2c2b458628cba8311be031d295dddb@ec2-52-17-81-195.eu-west-1.compute.amazonaws.com:5432/d9qjkvssoft7iu")
-
->>>>>>> 3f20fc0a7d2fe95c9fa5dca28593e16ad28d2ebc
 
 from database.login_database import *
 from database.menu_database import *
