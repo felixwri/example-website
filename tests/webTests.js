428a259b71ed3bb4ea67f366e0eff623316289cf
aais <zjac173@live.rhul.ac.uk>
2023-03-02 14:17:22 +0000

    Testing the cart update features when adding items

--- tests/webTests.js
+++ tests/webTests.js
@@ -16,3 +16,13 @@ test("page loads correctly", () => {
   expect(getByText("Welcome to My Page")).toBeInTheDocument();
 });
 
+// Case 3: When the user adds an item to the cart, the cart total should update with the new item price.
+
+test('Adding item should update cart total', () => {
+  const cart = new Cart();
+  const item = new Item('Product 1', 10);
+  addToCart(item, cart);
+  expect(cart.total).toBe(10);
+});
+
+
