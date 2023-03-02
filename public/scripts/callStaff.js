08dee797c1a4c8c169f84bf2f428268c98e094fb
Amad Chaudhari <amadchaudhari174@gmail.com>
2023-03-02 14:39:03 +0000

    added all staffCalls to a list

--- public/scripts/callStaff.js
+++ public/scripts/callStaff.js
@@ -1,4 +1,20 @@
-function callStaffReason(){
-    var reason = document.getElementById('requestStaff')
-    console.log(reason.value)
-}
+
+var calls = [];
+
+
+
+
+function logFormValues() {
+    let form = document.querySelector('form');
+    var reason = document.getElementById('requestStaff').value;
+
+    calls.push(reason);
+
+    console.log(calls);
+
+    for (let i = 0; i < calls.length; i++) {
+      console.log(calls[i]);
+    }
+
+    return false;
+  }
