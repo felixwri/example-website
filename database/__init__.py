50323d29413512d805e24abedd63ea36ff90f9de
felixwri <73856380+felixwri@users.noreply.github.com>
2023-03-07 17:39:53 +0000

    Database URI added to .env

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,9 @@
+import os
 import psycopg2
-connection = psycopg2.connect("postgres://obxjzakcoxgnuh:e1ee2bcadafbb550f8a7822769383fff92154bbfadac785bb0fec0ea1b3e928a@ec2-34-249-181-187.eu-west-1.compute.amazonaws.com:5432/ddrj5381iapr8u")
+from dotenv import load_dotenv
+
+load_dotenv()
+connection = psycopg2.connect(os.getenv('DATABASE_URI'))
 
 from .login_database import *
 from .menu_database import *
