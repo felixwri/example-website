2a13a0e9f08325bda790873d6e808513274eb9d1
dagsO <zkac213@live.rhul.ac.uk>
2023-03-09 22:47:08 +0000

    revised logout function to be more efficient

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template, send_from_directory, jsonify, session, redirect
 from flask_session import Session
 from flask_cors import CORS, cross_origin
+#import flask_login
 import database as db
 import string, random, secrets
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = secrets.token_hex(16)
@@ -52,12 +54,16 @@ def login():
 
         if db.check_password(username, password):
             session['username'] = 'staff'
+            session['loggedin'] = True
+
             return redirect('/staff/')
         else:
             return render_template("login.html", error = "Invalid Credentials")
 
 def logout():
-    session['username'] = ''
+    #removing session data will log out user
+    session.pop('loggedin', None)
+    session.pop('username', None)
     return redirect('/')
 
 
