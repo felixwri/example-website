71283649f226b79d6e49866486acb8e6d84998d6
aais <zjac173@live.rhul.ac.uk>
2023-03-14 17:11:08 +0000

    statusPiChart.js has been correctly commented

--- public/scripts/statusPiChart.js
+++ public/scripts/statusPiChart.js
@@ -1,3 +1,5 @@
+// The following is to create a pie chart to show the status of orders
+
 const Order = require("./order");
 
 google.charts.load('current', {'packages':['corechart']});
@@ -6,13 +8,13 @@ google.charts.setOnLoadCallback(drawChart);
 import Order from "./order";
 function drawChart() {
 
-
+  // Get the orders data from the server and parse it as a JSON object
   const orders = JSON.parse(` {{ orders | tojson }} `)
 
-
+  // Loop through each order in the orders array
   for (order in orders){
 
-    
+    // Loop through each item in the current order
     for (item in order["items"]){
       console.log("amad")
     }
@@ -35,7 +37,7 @@ function drawChart() {
 
 
 
-
+  // Set the chart options, including the chart title and dimensions
   var options = {'title':'Orders', 'width':700, 'height':500};
 
 
