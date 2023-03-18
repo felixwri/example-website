ef4403935d3bc3334c4571a241fdbe44a0eaea26
Rehnuma Rahman <rehnumarahman@rehnumas-mbp.home>
2023-03-18 10:31:26 +0000

    kids menu section complete with items, descriptions and their nutritional values

--- database/inserts.py
+++ database/inserts.py
@@ -31,12 +31,15 @@ def insert_into_menu(reset_table=False):
     coke_zero = 'https://res.cloudinary.com/djukm4fut/image/upload/v1678372077/oaxaca/Coke_Zero_qibtx1.png'
     #kids menu images
     beef_taco_cups = 'https://res.cloudinary.com/djukm4fut/image/upload/v1677854073/oaxaca/eiqeecrmuinmlfsimvqz.png'
-
-
+    mac_and_cheese = 'https://res.cloudinary.com/djukm4fut/image/upload/v1677854073/oaxaca/eiqeecrmuinmlfsimvqz.png'
+    chicken_nuggets = 'https://res.cloudinary.com/djukm4fut/image/upload/v1677854073/oaxaca/eiqeecrmuinmlfsimvqz.png'
+    taco_bites = 'https://res.cloudinary.com/djukm4fut/image/upload/v1677854073/oaxaca/eiqeecrmuinmlfsimvqz.png'
+    bean_burritos = 'https://res.cloudinary.com/djukm4fut/image/upload/v1677854073/oaxaca/eiqeecrmuinmlfsimvqz.png'
+    quesadillas = 'https://res.cloudinary.com/djukm4fut/image/upload/v1677854073/oaxaca/eiqeecrmuinmlfsimvqz.png'
+    
     #Starters
-    db.add_item('Tortilla Chips & Salsas', tortilla_chips_salsa, 'Starters', 5.50, 150, True, 'None', 'Served with our homemade roasted salsa and pico de gallo.')
-    db.add_item('Tortilla Chips & Guacamole', tortilla_chips_guacamole, 'Starters', 6.00, 200, True,'None', 'Our famous guacamole is freshly made every day using fresh ingredients.')
-    db.add_item('Nachos', 'Starters', default_image, 7.00, 306, True, 'Cheese, Jalapeños', 'Tortilla chips topped with jack cheese, roasted salsa, jalapeño cream cheese, sour cream and our famous guacamole.')
+    db.add_item('Tortilla Chips & Salsas', tortilla_chips_salsa, 'Starters', 5.50, 150, True, 'None', 'Tortilla chips served with our homemade roasted salsa and pico de gallo.')
+    db.add_item('Tortilla Chips & Guacamole', tortilla_chips_guacamole, 'Starters', 6.00, 200, True,'None', 'Tortilla chips topped with jack cheese, roasted salsa, jalapeño cream cheese, sour cream and our famous guacamole.')
     db.add_item('Southwest Spring Rolls', southwest_rolls,'Starters', 7.00, 239, True, 'Cheese', 'Chipotle Mexican chicken, peppers & jack cheese folded in a flour tortilla, deep fried & served with our famous guacamole.')
     db.add_item('Grilled Chicken Wings', grilled_wings, 'Starters', 7.50, 312, False, 'None', 'Grilled chicken with Lousiana Hot sauce.')
     #Mains
@@ -44,8 +47,8 @@ def insert_into_menu(reset_table=False):
     db.add_item('Seafood Paella', seafood_paella, 'Mains', 15.00, 200, False, 'Prawns, Nuts', 'Mexican rice cooked with chorizo, seafood cocktail, black tiger prawns, roasted salsa, peas, sweet corn and fresh coriander. (Hot)')
     db.add_item('Meat Paella', meat_paella, 'Mains', 15.00, 400, False, 'None', 'Slow cooked Mexican rice with chicken, chorizo, red salsa, peas & sweetcorn topped with grilled steak & bacon. (Hot)')
     db.add_item('Beef Carnitas Enchilada', enchiladas, 'Mains', 14.50, 410, False, 'None', 'Rolled flour or white corn tortillas (GF) filled with jack cheese, poblano chilli peppers and topped with creamy salsa, jalapeños & cheese. Oven baked and served over a bed of Mexican rice & our famous guacamole.')
-    db.add_item('Poblano Chicken', poblano_chicken, 'Mains', 14.50, 350, False, 'None', 'Grilled chicken breast topped with red salsa, poblano chilli peppers, melted jack cheese, served with seasoned fries.')
-    db.add_item('Mex Burger', mex_burger, 'Mains', 14.50, 600, False, 'Cheese', 'Beef burger folded in a flour tortilla with jack cheese, poblano chilli peppers & Chilli con Carne, served with pico de gallo, sour cream, our famous guacamole & Mexican rice.')
+    db.add_item('Poblano Chicken', poblano_chicken, 'Mains', 14.50, 350, False, 'None', 'Grilled chicken breast topped with red salsa, poblano chilli peppers, melted jack cheese, served with seasoned rice.')
+    db.add_item('Mex Burger', mex_burger, 'Mains', 14.50, 600, False, 'Cheese', 'Beef burger folded in corn flour buns with jack cheese, poblano chilli peppers & Chilli con Carne, served with pico de gallo, sour cream, our famous guacamole & Mexican rice.')
     #Desserts
     db.add_item('Ice Cream', icecream, 'Desserts', 4.00, 145, False, 'Milk', 'Two scoops, choose from vanilla, strawberry, chocolate, mint, mango or toffee.')
     db.add_item('Bunuelos', bunuelos, 'Desserts', 6.00, 212, False, 'None', 'Fried dough ball dusted with cinnamon sugar served with Dulce de Leche & chocolate fudge sauce.')
@@ -53,18 +56,24 @@ def insert_into_menu(reset_table=False):
     db.add_item('Churros', churros, 'Desserts', 6.50, 256, False, 'Milk', 'Mexican doughnut fried and dusted with cinnamon sugar with a side of Dulce de Leche & vanilla ice cream.')
     db.add_item('Banana Chimichanga', chimichanga, 'Desserts', 6.50, 200, False, 'Milk', 'A cinnamon flour tortilla filled with banana, deep fried and served with Dulce de Leche & vanilla ice cream.')
     #Soft Drinks, Beers, Ciders
-    db.add_item("Coke Zero", coke_zero, "Soft Drinks", 3.30, 0, None, None, None)
+    db.add_item("Coke Zero", coke_zero, "Soft Drinks", 3.30, 2, None, None, None)
     db.add_item("Coca-Cola", coca_cola, "Soft Drinks", 3.50, 139, None, None, None)
     db.add_item("Sprite", sprite, "Soft Drinks", 3.30, 110, None, None, None)
     db.add_item("Fanta", fanta, "Soft Drinks", 3.30, 120, None, None, None)
     db.add_item("Orange Juice", default_image, "Soft Drinks", 4.00, 30, None, None, None)
+    db.add_item("Apple Juice", default_image, "Soft Drinks", 4.00, 30, None, None, None)
     db.add_item("Budweiser 4.5% ABV", default_image, "Beers", 5.50, 186, None, None, None)
     db.add_item("Mahou 5.1% ABV", default_image, "Beers", 6.50, 190, None, None, None)
     db.add_item("Corona 4.5% ABV", default_image, "Beers", 5.50, 175, None, None, None)
-    db.add_item("Stella Cidre", default_image, "Ciders", 6.50, 150, None, None, None)
-    db.add_item("Stella Cidre Raspberry", default_image, "Ciders", 7.00, 190, None, None, None)
+    db.add_item("Stella Cider", default_image, "Ciders", 6.50, 150, None, None, None)
+    db.add_item("Stella Cider Raspberry", default_image, "Ciders", 7.00, 190, None, None, None)
     #Kids Menu
-    db.add_item('Beef Taco Cups', beef_taco_cups, 'Kids', 6.00, 300, False, 'Prawns', 'Slow cooked Mexican rice with tiger prawns, chicken, chorizo, red salsa, peas & sweetcorn. (Hot)')
+    db.add_item('Beef Taco Cups', beef_taco_cups, 'Kids', 7.00, 378, False, 'Soft taco cups layered with minced beef, sliced bell peppers, jalapeños, jack cheese, sour cream and tomatoes.')
+    db.add_item('Nacho Mac and Cheese', mac_and_cheese, 'Kids', 6.00, 346, False, 'Cheese', 'A classic macaroni and cheese dish, made with parmesan and chedder cheese.')
+    db.add_item('Chicken Nuggets and Wedges', chicken_nuggets, 'Kids', 6.00, 280, False, 'Air-fried chicken nuggets with a side of lightly spiced wedges, served with our chef-prepared tomato ketchup.')
+    db.add_item('Taco Fish Bites with Tomato Salsa', taco_bites, 'Kids', 7.00, 317, False, 'Fish', 'A mix of cod, salmon and seabass, enclosed within corn tortilla taco shells, topped with Monterey Jack cheese, avocado slices, bell peppers and red onions.')
+    db.add_item('Chicken and Bean Burritos', bean_burritos, 'Kids', 5.50, 253, False, 'Toasted tortilla wrap with a filling of white rice, black turtle beans, red bell peppers, cheese and 100% chicken breast.')
+    db.add_item('Mini Quesadillas', quesadillas, 'Kids', 6.00, 320, False, 'Cheese', 'Milk', 'Oaxaca cheese and mozarella filled tortilla wraps, toasted and served with a sour cream.')
 
 def insert_admin_account():
     db.add_user("test", "password1234#")
